d11adcf1bf208efa79627ab4b787512272d89840
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-01-19 14:58:12 +0100

    third

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
+
+//1
 const evowels = (str)=>{
     
     let newv = str.split("");
     let counter = 0;
     for (let i=0; i< newv.length; i++){
-        if (i == newv[i]){counter++;}   
+        newv[i] += counter++;
     }
     return counter;
 };
 console.log(evowels("a,o,u,i,e"));
-
+//2
 const set = (arr)=>{
     let result = "";
     for (let i=0; i< arr.length; i++){
         if (i == arr[i]){result.splice.repeat(arr[i]);}
     }
     return result;
-}
+};
 
 console.log(set[1,6,7,5,7,1]);
-
+//3
 const dictionary = (str, arry)=>{
 let result = "";
     for (let i=0; i< str.length; i++)
